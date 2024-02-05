1e6c20f7e88feea44985e6dacbddc3c4d5701e3e
waxnkw <zhanga6@outlook.com>
2024-02-05 11:18:30 +0800

    add deepspeed

---
+++ config/nextchat_stage1_deepspeed.py
@@ -0,0 +1,13 @@
+_base_ = ['_base_/dataset/nextchat_stage1.py', '_base_/model/nextchat.py', '_base_/train/nextchat_deepspeed.py']
+
+training_args = dict(
+    num_train_epochs=2,
+    output_dir='./exp/{{fileBasenameNoExtension}}',
+)
+
+model_args = dict(
+    conv_args=dict(
+        tokenize_kwargs=dict(truncation_size=4096),
+    ),
+    model_name_or_path=None,
+)
--- run_stage1.sh
+++ run_stage1.sh
@@ -3,6 +3,6 @@ accelerate launch --num_processes 8 \
         mllm/pipeline/finetune.py \
         config/nextchat_stage1.py \
         --cfg-options model_args.model_name_or_path=/data/public/multimodal/multimodal_model_ckpts/vicuna-7b-v1.5 \
-        model_args.mm_projector_depth=2 model_args.pretrained_mm_projector=/home/zhangao/model/llava/projector/vicuna1.5_7b/mm_projector.bin \
+        model_args.mm_projector_depth=2 \
         --num_train_epochs 2 \
         --output_dir ./output/stage1
